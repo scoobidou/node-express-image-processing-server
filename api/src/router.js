482d9d1d69758a911957f0ac9f30f9782c6d81cd
scoobidou <emmanuel.croccel@outlook.fr>
2021-04-15 14:49:25 +0200

    Module 3 - checking

--- api/src/router.js
+++ api/src/router.js
@@ -1,7 +1,10 @@
 const express = require ('express');
 const multer = require ('multer');
+const path = require ('path');
 
 const router = express.Router();
+
+const photoPath = path.resolve(__dirname,'../../client/photo-viewer.html');
 
 function filename(resquet,file,callback){
     callback(null,file.originalname);
@@ -22,6 +25,10 @@ const fileFilter = (request,file,callback) => {
 };
 
 const upload = multer({'fileFilter':fileFilter,'storage': storage});
+
+router.get('/photo-viewer', (request,response) =>{
+    response.sendFile(photoPath);
+});
 
 router.post('/upload', upload.single('photo'), (request, response) => {
   if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
