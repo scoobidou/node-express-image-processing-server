936912ed5600944852244d2abf4923babc3f2821
scoobidou <emmanuel.croccel@outlook.fr>
2021-04-15 14:18:53 +0200

    Module 2 - Check

---
+++ api/src/router.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+const multer = require ('multer');
+
+const router = express.Router();
+
+function filename(resquet,file,callback){
+    callback(null,file.originalname);
+}
+const storage = multer.diskStorage({
+    destination: 'api/uploads/',
+    filename: filename
+});
+
+
+const fileFilter = (request,file,callback) => {
+    if(file.mimetype !== 'image/png'){
+        request.fileValidationError = 'Wrong file type';
+        callback(null, false, new Error('Wrong file type'));
+    }else{
+        callback(null,true);
+    }
+};
+
+const upload = multer({'fileFilter':fileFilter,'storage': storage});
+
+router.post('/upload', upload.single('photo'), (request, response) => {
+  if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
+
+  return response.status(201).json({success: true});
+  });
+
+module.exports = router;
+
